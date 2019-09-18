ba574f03f02d53c99987bc427494529fda60f450
zseibert <ZESeibert@gmail.con>
2019-09-18 17:44:41 -0400

    Assignment changes

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,28 +1,36 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
-/* Connect to your database using mongoose - remember to keep your key secret*/
-//see https://mongoosejs.com/docs/connections.html
-//See https://docs.atlas.mongodb.com/driver-connection/
+// Remove all deprecation warnings
+mongoose.connect(config.db.uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+});
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
-  //see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-
-  Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
+// Read in listings.json file
+fs.readFile('listings.json', 'utf8', function(err, data) {
+    if (err) throw err;
 
-
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+    // Create storing variable
+    var listings = JSON.parse(data);
+    
+    // Creates array of all listing data
+    listings.entries.forEach(function(listing) {
+	console.log(listing);
+	new Listing(listing).save();
+    });
+});
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri:'mongodb+srv://zseibert:Caro9898@cluster0-fh82j.mongodb.net/test?retryWrites=true&w=majority'
+  }
+};
